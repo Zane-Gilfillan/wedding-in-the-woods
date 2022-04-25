12c270bd87f2e133ba6d534be82d248d8e5a6ace
Zane-Gilfillan <zane.gilfillan@gmail.com>
2022-04-25 18:46:26 -0500

    swapped form component for a notes function. needs styling

--- wedding-site/pages/rsvp.js
+++ wedding-site/pages/rsvp.js
@@ -1,32 +1,46 @@
 import React from 'react';
 import { useState } from 'react';
+import produce from 'immer';
 import { v4 as uuidv4 } from "uuid";
 import Image from 'next/image';
 import Link from 'next/link';
 import Nav from '../components/header';
 import invite from '../public/invite.jpg';
 import styles from '../styles/Rsvp.module.scss';
+import { useEffect } from 'react';
+
+const Notes = props => props.data.map(note => <div>{note.text}</div>);
 
 const rsvp = () => {
 
-    const [item, setItem] = useState('');
-    const [items, setItems] = useState([]);
-    const add = (e) => {
-        e.preventDefault();
-        if (!item) {
-            return;
+    const initialData =[{ text: ''}]
+    const [data, setData] = useState(initialData);
+
+    const handleClick = () => {
+        const text = document.querySelector('#noteinput').value.trim();
+        if (text) {
+            const nextState = produce(data, draftState => {
+                draftState.push({ text });
+            });
+            document.querySelector('#noteinput').value = '';
+            
+            if (typeof window !== 'undefined') {
+                localStorage.setItem('data', JSON.stringify(nextState));
+            };
+
+            setData(nextState);
         };
-        setItems((items) => [
-            ...items,
-            {
-                id: uuidv4(),
-                item
-            }
-        ]);
     };
-    const remove = (index) => {
-        setItems((items) => items.filter((_, i) => i !== index));
-    }
+
+    useEffect(() => {
+        const getData = localStorage.getItem('data');
+
+        if (getData !== '' && getData !== null) {
+            return setData(JSON.parse(getData));
+        }
+        return setData([]);
+    }, 0);
+
   return (
     <>
         <Nav />
@@ -34,31 +48,13 @@ const rsvp = () => {
 
             <Image src= {invite} className={styles.img} alt='cabin in the woods' />
             <div className={styles.list__holder}>
-                <div className={styles.text}>names go here</div>
-                <div className={styles.button__container}>
-                <Link href='/rsvp'><div className={styles.btn}>rsvp</div></Link>
+                <div className="note__container">
+                    <input id='noteinput' placeholder='leave your rsvp here' type="text" />
+                    <button onClick={() => handleClick()}>rsvp</button>
+                    <Notes data={data} />
                 </div>
             </div>
-            <div className="App">
-                <form onSubmit={add}>
-                    <fieldset>
-                    <label>item</label>
-                    <input value={item} onChange={(e) => setItem(e.target.value)} />
-                    </fieldset>
-                    <button type="submit">add item</button>
-                </form>
-                {items.map((item, index) => {
-                    return (
-                    <div key={item.id}>
-                        <p>{item.item}</p>
-                        <button onClick={() => remove(index)}>remove</button>
-                    </div>
-                    );
-                })}
-                </div>
-            
         </div>
-
     </>
   )
 }
